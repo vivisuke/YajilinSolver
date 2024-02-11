91b3495c9750f59ae3c1f4b9cab54a5667eef67d
vivisuke <ntsuda@master.email.ne.jp>
2024-02-11 11:45:34 +0900

    0005 ◎ C 02/19 02/11 0.001│ToDo│盤面各セルにラベル設置

--- YajilinSolver/scripts/main.gd
+++ YajilinSolver/scripts/main.gd
@@ -1,15 +1,38 @@
 extends Node2D
 
+enum {
+	TILE_NONE = -1,
+	TILE_CURSOR,			# カーソル（薄ピンク）
+	TILE_ARROWS,			# 
+	TILE_ARROWS2,			# 
+}
+
 const N_HORZ = 10
 const N_VERT = 10
-const TILE_NONE = -1
-const TILE_CURSOR = 0					# カーソル（薄ピンク）
+const CELL_WIDTH = 49
 
 var cur_pos = Vector2i(-1, -1)
+var num_labels = []
 
 func _ready():
+	init_labels()
+	for y in range(N_VERT):
+		for x in range(N_HORZ):
+			var t = TILE_ARROWS if (x+y)%2 == 0 else TILE_ARROWS2
+			$Board/TileMap.set_cell(0, Vector2i(x, y), t, Vector2i(0, 0))
 	pass # Replace with function body.
-
+func init_labels():
+	for y in range(N_VERT):
+		var py = y * CELL_WIDTH + 2
+		for x in range(N_HORZ):
+			var px = x * CELL_WIDTH + 10
+			var label = Label.new()
+			num_labels.push_back(label)
+			label.add_theme_color_override("font_color", Color.BLACK)
+			label.add_theme_font_size_override("font_size", 40)
+			label.position = Vector2(px, py)
+			label.text = str((x+y)%10)
+			$Board.add_child(label)
 func _process(delta):
 	pass
 
